9b96258d077e24ddb367220c1881c17fb630cb88
kunal <monupanwar5655@gmail.com>
2025-05-19 18:36:38 +0530

    custom input added

---
+++ frontend/src/components/Input.tsx
@@ -0,0 +1,17 @@
+interface InputProps{
+  placeholder:string,
+  refrence?:any
+}
+
+export default function Input({placeholder,refrence}:InputProps) {
+  return (
+    <div>
+      <input 
+      ref={refrence}
+      placeholder={placeholder}
+      type={"text"}
+     className="px-4 py-2 rounded-md m-2" 
+      />
+    </div>
+  )
+}
