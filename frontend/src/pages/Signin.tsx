c7704e38776e5820f2b625607786f9f30fea4891
kunal <monupanwar5655@gmail.com>
2025-05-19 17:58:34 +0530

    sadcn ui added

---
+++ frontend/src/pages/Signin.tsx
@@ -0,0 +1,15 @@
+export default function Signin() {
+  return (
+    <div className="h-screen w-full bg-gray-200 flex  items-center justify-center">
+      <div className="bg-white rounded-xl border p-8">
+        <input placeholder="username" />
+        <input placeholder="password" />
+         
+         <div>
+         
+         </div>
+      </div>
+
+    </div>
+  )
+}
